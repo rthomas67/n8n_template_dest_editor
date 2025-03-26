fb4b338cc20a4f24466a3b59ab6b6492024a86d4
rrt <rrt_gogs@liveintellect.org>
2025-03-25 21:01:49 -0600

    working but still needs some finishing work

--- EditN8nTemplateDestinations.js
+++ EditN8nTemplateDestinations.js
@@ -5,7 +5,7 @@
 // @description  Add a little editor to manage Template Destinations
 // @author       Someone Awesome
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @match        https://n8n.io/workflows/
+// @match        https://n8n.io/workflows*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=n8n.io
 // @grant        none
 // @run-at       document-idle
@@ -31,10 +31,13 @@
 
     function addTemplateEditorCSS() {
         var cssString =
-            '.template-editor-root {margin: 0px;padding: 0px; flex: left}' +
-            '.template-editor-list {margin: 0px;padding: 0px;}' +
-            '.template-editor-button { text-align: center; padding: 8px 12px; cursor: pointer; color: white; background-color: #d54b00; margin: 4px 2px; font-size: 16px; font-weight: bold }' +
-            '.template-editor-body {padding: 5px 10px 15px;background-color:#F4F4F8;}'
+            '.template-dest-editor-root {margin: 0px;padding: 0px; flex: left}\n' +
+            '.template-dest-editor-button { display: flex; text-align: center; justify-content: space-between; padding: 2rem 24px; cursor: pointer; color: white; background-color: #1c9985; width: 100%; margin-top: 4px; font-size: 1.8rem; font-family: moderat; border-radius: 8px; border: 2px solid #1c9985;}\n' +
+            '.template-dest-editor-body { padding: 5px 10px 15px; margin-top: 4px; background-color:#BBDEFB; position: relative; width: 100%; min-width: 150px; min-height: 200px;}\n' +
+            '.template-dest-editor-form { width: 100%; }\n' +
+            '.template-dest-editor-layout { display: flex; flex-direction: column; justify-content: space-around; align-items: stretch; width: 100%; }\n' +
+            '.template-dest-editor-textarea { min-height: 150px; width: 100%; }\n' +
+            '.template-dest-editor-save-button { width: 25%; }\n'
             ;
         var head = document.getElementsByTagName('head')[0];
         if (!head) {
@@ -47,28 +50,77 @@
         head.appendChild(newCss);
     }
 
+    var allDoneMutationObserver = new MutationObserver(resetTimer);
+    var timer = setTimeout(action, 3000, allDoneMutationObserver);
+    // reset timer every time something changes
+    function resetTimer(changes, allDoneMutationObserver) {
+        clearTimeout(timer);
+        timer = setTimeout(action, 3000, allDoneMutationObserver);
+    }
+
+    function action(observer) {
+        allDoneMutationObserver.disconnect();
+        addTemplateEditor();
+    }
+
+    function loadTemplateDestinations() {
+        let templateDestinations = getStringArrayFromStorage('N8N_TEMPLATE_DESTINATIONS');
+        console.log(JSON.stringify(templateDestinations));
+        return templateDestinations;
+    }
+
+    function saveTemplateDestinations() {
+        var templateDestinationsTextAreaValue = $('.template-dest-editor-textarea').val();
+        console.log(templateDestinationsTextAreaValue);
+        // TODO: clean up some editing mistakes (extra linefeed, blank lines, etc.)
+        var templateDestinationsArray = templateDestinationsTextAreaValue.split('\n');
+        updateStringArrayToStorage('N8N_TEMPLATE_DESTINATIONS', templateDestinationsArray);
+        toggleEditableFormList();
+    }
+
+    function buildEditableListForm(arrayOfItems) {
+        var editableListFormDiv = $('<div id="editable-list-form" class="template-dest-editor-body" />');
+        // var editableListForm = $('<form class="template-dest-editor-form" />');
+        // editableListFormDiv.append(editableListForm);
+        var editableFormLayoutContainer=$('<div class="template-dest-editor-layout" />');
+        // editableListForm.append(editableFormLayoutContainer);
+        editableListFormDiv.append(editableFormLayoutContainer);
+        var textArea = $('<textarea class="template-dest-editor-textarea" />');
+        editableFormLayoutContainer.append(textArea);
+        var textAreaContent = arrayOfItems.join('\n');
+        textArea.val(textAreaContent);
+        var saveButton = $('<button class="template-dest-editor-save-button">Save</button>');
+        saveButton.click(saveTemplateDestinations);
+        editableFormLayoutContainer.append(saveButton);
+        return editableListFormDiv;
+    }
+
+    function toggleEditableFormList() {
+        let listForm = $('#editable-list-form');
+        // console.log(listForm);
+        listForm.toggle();
+    }
+
     function addTemplateEditor() {
         addTemplateEditorCSS();
 
         var templateDestEditorUI = {};
-        templateDestEditorUI.editorRoot = $('<div class="template-editor-root"/>');
-        templateDestEditorUI.editorPopupButton = $('<button class="template-editor-button">Modify Template Destinations</button>');
+        templateDestEditorUI.editorRoot = $('<div class="template-dest-editor-root"/>');
+        templateDestEditorUI.editorPopupButton = $('<button class="template-dest-editor-button">Modify Template Destinations</button>')
+        templateDestEditorUI.editorPopupButton.click(toggleEditableFormList);
         templateDestEditorUI.editorRoot.append(templateDestEditorUI.editorPopupButton);
 
-        // This breaks other JS on the page somehow
-        var pageAttachElementLogo = $('.header__wrapper-logo');
-        console.log(pageAttachElementLogo);
-        pageAttachElementLogo.after(templateDestEditorUI.editorRoot);
+        var templateDestinations = loadTemplateDestinations();
+        templateDestEditorUI.editableListForm = buildEditableListForm(templateDestinations);
+        templateDestEditorUI.editableListForm.hide();
+        templateDestEditorUI.editorRoot.append(templateDestEditorUI.editableListForm);
 
-        // var pageAttachElement = document.getElementsByClassName('header__wrapper-row')[0];
-        // var pageAttachElement = $('.header__wrapper-row');
-        // var pageAttachElement = $('.header  ');
-        // pageAttachElement.append(templateDestEditorUI.editorRoot);
+        // Note: This doesn't work without the allDoneMutationObserver
+        var pageAttachElementUseWorkflowButtonContainer = $('[class^="use-template-container_"]');
+        if (pageAttachElementUseWorkflowButtonContainer) {
+            console.log(pageAttachElementUseWorkflowButtonContainer);
+            pageAttachElementUseWorkflowButtonContainer.append(templateDestEditorUI.editorRoot);
+        }
     }
-
-    var templateDestinations = getStringArrayFromStorage('N8N_TEMPLATE_DESTINATIONS');
-    console.log(JSON.stringify(templateDestinations));
-    addTemplateEditor();
-
 
 })();
